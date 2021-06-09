2ba5c422d2bc5a17fd73adf414b165b22a456547
sujay_nagvekar <sujay.nagvekar@capitissolutions.com>
2021-06-09 02:08:25 -0500

    spotify_api move from kaggle to google storage code

---
+++ spotify_api/read_csv_from_gs.py
@@ -0,0 +1,15 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="F:\Road to Opta\directed-post-316003-300502ca1f7e.json"
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
+
+implicit()
