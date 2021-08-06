9e0a834b799c2c541161a162ed2d74931e3c1a58
sujay_nagvekar <sujay.nagvekar@capitissolutions.com>
2021-08-06 13:41:52 -0500

    main.py file and Dockerfile

--- docker/main.py
+++ docker/main.py
@@ -3,13 +3,14 @@ import pandas as pd
 from pydantic import BaseModel
 from operator import itemgetter
 import json
+import uvicorn
 # os.chdir("F:\\Road to Opta\\player_stats\\")
 
 #
 app = FastAPI()
 
-
-mean_stats = pd.read_csv("F:\\Road to Opta\\player_stats\\scaled_mean_features.csv")
+mean_stats = pd.read_csv("/usr/src/app/data/scaled_mean_features.csv")
+# mean_stats = pd.read_csv("F:\\Road to Opta\\player_stats\\scaled_mean_features.csv")
 mean_stats = mean_stats.iloc[:, 1:]
 
 
@@ -22,9 +23,9 @@ lowercase(mean_stats)
 final_columns = mean_stats.player_name.to_list()
 
 
-def cosine_similarity_df(x, top, sp):
+def cosine_similarity_df(x, top, sp, df):
     from sklearn.metrics.pairwise import cosine_similarity
-    cosine = cosine_similarity(x.iloc[:, 1:], mean_stats.iloc[:, 1:])
+    cosine = cosine_similarity(x.iloc[:, 1:], df.iloc[:, 1:])
     cosine_df = pd.DataFrame(cosine, index=x.iloc[:, 1:].index)
     cosine_df.columns = final_columns
     cosine_df = pd.concat([x.player_name, cosine_df], axis=1)
@@ -33,9 +34,9 @@ def cosine_similarity_df(x, top, sp):
     return cos_top_10
 
 
-def euclid_similarity_df(x, top, sp):
+def euclid_similarity_df(x, top, sp, df):
     from scipy.spatial.distance import cdist
-    euclid = cdist(x.iloc[:, 1:], mean_stats.iloc[:, 1:], 'euclid')
+    euclid = cdist(x.iloc[:, 1:], df.iloc[:, 1:], 'euclid')
     euclid_df = pd.DataFrame(euclid, index=x.iloc[:, 1:].index)
     euclid_df.columns = final_columns
     euclid_df = pd.concat([x.player_name, euclid_df], axis=1)
@@ -44,9 +45,9 @@ def euclid_similarity_df(x, top, sp):
     return euc_top_10
 
 
-def manhattan_similarity_df(x, top, sp):
+def manhattan_similarity_df(x, top, sp, df):
     from scipy.spatial.distance import cdist
-    manhattan = cdist(x.iloc[:, 1:], mean_stats.iloc[:, 1:], 'cityblock')
+    manhattan = cdist(x.iloc[:, 1:], df.iloc[:, 1:], 'cityblock')
     manhattan_df = pd.DataFrame(manhattan, index=x.iloc[:, 1:].index)
     manhattan_df.columns = final_columns
     manhattan_df = pd.concat([x.player_name, manhattan_df], axis=1)
@@ -55,9 +56,9 @@ def manhattan_similarity_df(x, top, sp):
     return man_top_10
 
 
-def minkowski_similarity_df(x, top, sp):
+def minkowski_similarity_df(x, top, sp, df):
     from scipy.spatial.distance import cdist
-    minkowski = cdist(x.iloc[:, 1:], mean_stats.iloc[:, 1:], 'minkowski')
+    minkowski = cdist(x.iloc[:, 1:], df.iloc[:, 1:], 'minkowski')
     minkowski_df = pd.DataFrame(minkowski, index=x.iloc[:, 1:].index)
     minkowski_df.columns = final_columns
     minkowski_df = pd.concat([x.player_name, minkowski_df], axis=1)
@@ -91,18 +92,16 @@ class SearchPlayer(BaseModel):
     name: str
 
 
-@app.get('/stats_list')
-async def stats():
-    return stats
-
+class ChangeStats(BaseModel):
+    list_of_stats: list
 
 @app.post('/similar_players')
 async def similar_player(sp: SimilarPlayer):
     similar_df = mean_stats.loc[mean_stats['player_name'] == sp.player.lower()]
-    cos_top_10 = cosine_similarity_df(similar_df, sp.select_top, sp.player.lower())
-    euc_top_10 = euclid_similarity_df(similar_df, sp.select_top, sp.player.lower())
-    man_top_10 = manhattan_similarity_df(similar_df, sp.select_top, sp.player.lower())
-    mink_top_10 = minkowski_similarity_df(similar_df, sp.select_top, sp.player.lower())
+    cos_top_10 = cosine_similarity_df(similar_df, sp.select_top, sp.player.lower(), mean_stats)
+    euc_top_10 = euclid_similarity_df(similar_df, sp.select_top, sp.player.lower(), mean_stats)
+    man_top_10 = manhattan_similarity_df(similar_df, sp.select_top, sp.player.lower(), mean_stats)
+    mink_top_10 = minkowski_similarity_df(similar_df, sp.select_top, sp.player.lower(), mean_stats)
     dict_of_players = similarity(cos_top_10, euc_top_10, man_top_10, mink_top_10, sp.select_top)
     final_list = []
     for i in getlist(dict_of_players):
@@ -119,7 +118,38 @@ async def search_player(search: SearchPlayer):
 
 @app.get('/stats_used')
 async def stats():
-    with open('F:\\Road to Opta\\ml-ops\\legends.json') as f:
+    # with open('F:\\Road to Opta\\ml-ops\\legends.json') as f:
+    #         stats = json.load(f)
+    # # columns = mean_stats.columns
+    return mean_stats.columns.to_list()
+
+
+@app.get('/legends')
+async def stats():
+    with open('/usr/src/app/data/legends.json') as f:
             stats = json.load(f)
-    # columns = mean_stats.columns
     return stats
+
+
+@app.post('/change_stats')
+async def stats(change: ChangeStats, sp: SimilarPlayer):
+    temp_df = pd.read_csv("/usr/src/app/data/scaled_mean_features.csv")
+    temp_df = temp_df.iloc[:, 1:]
+    lowercase(temp_df)
+    temp_df = temp_df[[*change.list_of_stats]]
+    final_columns = temp_df.player_name.to_list()
+    similar_df = temp_df.loc[temp_df['player_name'] == sp.player.lower()]
+    cos_top_10 = cosine_similarity_df(similar_df, sp.select_top, sp.player.lower(), temp_df)
+    euc_top_10 = euclid_similarity_df(similar_df, sp.select_top, sp.player.lower(), temp_df)
+    man_top_10 = manhattan_similarity_df(similar_df, sp.select_top, sp.player.lower(), temp_df)
+    mink_top_10 = minkowski_similarity_df(similar_df, sp.select_top, sp.player.lower(), temp_df)
+    dict_of_players = similarity(cos_top_10, euc_top_10, man_top_10, mink_top_10, sp.select_top)
+    final_list = []
+    for i in getlist(dict_of_players):
+        i = i.title()
+        final_list.append(i)
+    return {'similar_players': final_list}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host="0.0.0.0")
