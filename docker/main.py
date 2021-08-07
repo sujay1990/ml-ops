3b12b15dda4af951ab2c8d34b41f876f4b097001
sujay_nagvekar <sujay.nagvekar@capitissolutions.com>
2021-08-07 01:41:30 -0500

    k8s deployment.yaml and service.yaml files, Dockerfile port exposed

--- docker/main.py
+++ docker/main.py
@@ -152,4 +152,4 @@ async def stats(change: ChangeStats, sp: SimilarPlayer):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host="0.0.0.0")
+    uvicorn.run(app, port=5000, host="0.0.0.0")
